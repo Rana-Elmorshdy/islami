36cb10f12b5f6232270c4096e2a957f5dcc83432
Rana Elmorshdy <rana2saad22@gmail.com>
2025-09-13 20:58:01 +0300

    + Initialize project.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,6 +3,5 @@ class AppColors {
   static const Color primaryColor = Color(0xFFE2BE7F);
   static const Color blackColor = Color(0xFF000000);
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color blackBgColor = Color(0x99202020);
-  
+  static const Color blackBgColor = Color(0x99202020);  
 }
